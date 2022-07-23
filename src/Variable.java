f1e7c82da5cc205dd2842d6167f6bd672167201b
herman-xphp <hermanxphp@gmail.com>
2022-07-23 15:27:37 +0800

    variable

---
+++ src/Variable.java
@@ -0,0 +1,35 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        String name;
+        name = "Herman";
+
+        System.out.println(name);
+
+        int age = 21;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Anto";
+        System.out.println(name);
+
+        // semenjak java versi 10
+        // kata kunci var
+
+        var firstName = "Herman";
+        var lastName = "Xphp";
+
+        // var company;
+        // company = "Programmer Copas";
+
+        // kata kunci final == konstan
+        final String application = "Belajar Java";
+
+        // application = "Belajar Kotlin"; // error
+
+
+
+    }
+}
