28cc5a6eb0e8a5e2e09f92d5c7ca72d0175c313c
herman-xphp <hermanxphp@gmail.com>
2022-07-23 11:11:16 +0800

    tipe data number

---
+++ src/KonversiNumber.java
@@ -0,0 +1,14 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+        // konversi tipe data number
+        // auto
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInteger = iniShort;
+
+        // manual
+        // int iniInteger2 = 1000;
+        byte iniByte2 = (byte) iniInteger;
+
+    }
+}
---
+++ src/TipeDataNumber.java
@@ -0,0 +1,24 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        // bulat
+        byte iniByte = 100;
+        short iniShort = 10000;
+        int integer = 1000000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000000000000L;
+
+        // pecahan
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        // literal
+        int decimalInt = 25;
+        int hexDecimal = 0xFFFFF;
+        int binaryDecimal = 0b1010101;
+
+        // underscore
+        long balance = 1_000_000_000L;
+        int sum = 60_000_000;
+
+    }
+}
