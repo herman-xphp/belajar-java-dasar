b833014d5cd0d6d3804188cb7051501f7567741d
herman-xphp <hermanxphp@gmail.com>
2022-07-23 11:56:49 +0800

    tipe data string

--- src/TipeDataString.java
+++ src/TipeDataString.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TipeDataString {
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Herman";
+        String lastName = "Xphp";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
 }
