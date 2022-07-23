511791b7a858ba2a968598a0ce7f4864444f79b1
herman-xphp <hermanxphp@gmail.com>
2022-07-23 16:34:59 +0800

    blok

---
+++ src/Block.java
@@ -0,0 +1,11 @@
+public class Block {
+  public static void main(String[] args) {
+    System.out.println("Hello World 1");
+
+    {
+      System.out.println("Hello World 1");
+      System.out.println("Hello World 1");
+    }
+
+  }
+}
