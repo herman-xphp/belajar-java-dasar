c9a4d964f3ebbe7b3673d0e64e00420582da32ff
herman-xphp <hermanxphp@gmail.com>
2022-07-23 11:50:26 +0800

    tipe data char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char m = 'M';
+        char a = 'A';
+        char n = 'N';
+
+        System.out.print(m);
+        System.out.print(a);
+        System.out.print(n);
+    }
+}
