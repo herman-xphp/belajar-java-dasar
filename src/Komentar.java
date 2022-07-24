fa19724e09fa0bce669ea16b3b3218c4507ee86c
herman-xphp <hermanxphp@gmail.com>
2022-07-24 15:26:37 +0800

    komentar

---
+++ src/Komentar.java
@@ -0,0 +1,18 @@
+public class Komentar {
+  public static void main(String[] args) {
+
+    System.out.println(sum(10, 20));
+
+  }
+
+  /**
+   * Method untuk menjumlahkan value1 dan value2
+   * @param value1 angka pertama
+   * @param value2 angka kedua
+   * @return hasil penjumlahan value1 dan value2
+   */
+  static int sum(int value1, int value2) {
+    // jumlahkan value1 dengan value2
+    return value1 + value2;
+  }
+}
