521d169813572b8a7af206f3b62338c76500a557
herman-xphp <hermanxphp@gmail.com>
2022-07-24 14:44:09 +0800

    method overloading

---
+++ src/MethodOverloading.java
@@ -0,0 +1,21 @@
+public class MethodOverloading {
+  public static void main(String[] args) {
+
+    sayHello();
+    sayHello("Herman");
+    sayHello("Herman", "Xphp");
+
+  }
+
+  static void sayHello() {
+    System.out.println("Hello");
+  }
+
+  static void sayHello(String name) {
+    System.out.println("Hello " + name);
+  }
+
+  static void sayHello(String firstName, String lastName) {
+    System.out.println("Hello " + firstName + " " + lastName);
+  }
+}
