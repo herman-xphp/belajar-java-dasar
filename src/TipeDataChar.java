9813b31a98e4e2f476da6c65835df00a1861fb4b
herman-xphp <hermanxphp@gmail.com>
2025-01-13 23:10:14 +0800

    belajar tipe data char dan string

---
+++ src/TipeDataChar.java
@@ -0,0 +1,12 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+
+        char m = 'M';
+        char a = 'A';
+        char n = 'N';
+
+        System.out.print(m);
+        System.out.print(a);
+        System.out.print(n);
+    }
+}
---
+++ src/TipeDataString.java
@@ -0,0 +1,11 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Herman";
+        String lastName = "Ganteng";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
